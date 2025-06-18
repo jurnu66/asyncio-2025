16c4d47c38570476d1f7414443f0c92627f051ee
jurnu66 <6610301011@cdti.ac.th>
2025-06-18 11:41:45 +0700

    new

--- assignment02/process03.py
+++ assignment02/process03.py
@@ -1,2 +1,49 @@
-# Multiprocessing 2 kitkens, 2 cooker, 2 dishes
-# 2 process
+# Thread version of cooking 1 kitchen 1 chefs 1 dishes
+import os
+from time import time, ctime, sleep
+import threading
+
+def cooking(index, basket):
+    print(f'{ctime()} Kitchen-{index} : Begin cooking...PID {os.getpid()}')
+    cooking_time = time()
+    print(f'{ctime()} Kitchen-{index} : Begin cooking...')
+    
+    # Use eggs from basket
+    basket.eat_egg(index)
+    
+    sleep(2)
+    duration = time() - cooking_time
+    print(f'{ctime()} Kitchen-{index} : Cooking done in {duration:0.2f} seconds!')
+
+class Basket:
+    def __init__(self):
+        self.eggs = 10
+        self.lock = threading.Lock()
+
+    def eat_egg(self, index):
+        with self.lock:
+            print(f'{ctime()} Kitchen-{index} : Chef-{index} has lock eggs remaining {self.eggs}')
+            self.eggs -= 1
+            print(f'{ctime()} Kitchen-{index} : Chef-{index} has released lock with eggs remaining {self.eggs}')
+
+if __name__ == "__main__":
+    # Begin of main thread
+    print(f'{ctime()} Main      : Starting cook.')
+    print(f'{ctime()} Main      : ID of main process: {os.getpid()}')
+    start_time = time()
+    
+    basket = Basket()
+    
+    # Multi thread cooking
+    chefs = list()
+    for index in range(5):
+        c = threading.Thread(target=cooking, args=(index, basket,))
+        chefs.append(c)
+        c.start()
+    
+    for index, c in enumerate(chefs):
+        c.join()
+    
+    print(f'{ctime()} Main      : Basket eggs remaining {basket.eggs}')
+    duration = time() - start_time
+    print(f"{ctime()} Main      : Finished Cooking duration in {duration:0.2f} seconds")
