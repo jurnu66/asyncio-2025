db1de2dfcf15c1cff0782839cef976d3a1b30e97
jurnu66 <6610301011@cdti.ac.th>
2025-07-02 11:22:54 +0700

    ex05-09

--- assignment05/asyncio02.py
+++ assignment05/asyncio02.py
@@ -1,1 +1,24 @@
-# example of waiting for the first task to complete
+import asyncio
+from random import random
+
+# Coroutine to execute in a new task
+async def task_coro(arg):
+    value = random()
+    await asyncio.sleep(value)
+    print(f'task {arg} done with {value}')
+    return value
+
+# Main coroutine
+async def main():
+    tasks = [asyncio.create_task(task_coro(i)) for i in range(10)]
+    
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    
+    print('Done')
+    
+    
+    first = done.pop()
+    print(f'First completed task result: {first.result()}')
+
+
+asyncio.run(main())
